420ee40c4a349643b18e31b5f5a58da24c6697a6
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-03 01:12:01 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,28 @@
  import express from "express";
 import { ENV } from "./lib/env.js";
- 
+import path from "path"
+import { fileURLToPath } from "url";
 
+//const __filename = fileURLToPath(import.meta.url);
+const __dirname = import.meta.dirname;
+const app=express();
 
- const app=express();
-
-
- app.get("/",(req,res)=>{
+ app.get("/health",(req,res)=>{
     res.status(200).json({msg:"success from api nodemon 123"})
  })
+
+ app.get("/books",(req,res)=>{
+   res.status(200).json({msg:"this is the book end point"})
+})
+
+if (ENV.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+ 
+   app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    });
+ }
+
  app.listen(ENV.PORT,()=>{
     console.log("server is running on port 3000")
  })
