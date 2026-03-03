cb75fe71a07e429976cc175a4a31b2cd37c37ede
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-04 01:08:57 +0530

    what is this

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ app.use("/api", apiRoutes);
 
 // 🔹 Serve frontend
 const __dirname = path.resolve();
+
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // 🔹 React router fallback (VERY IMPORTANT)
