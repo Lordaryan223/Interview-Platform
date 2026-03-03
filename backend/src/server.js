57cf6f9e5b34082f7e3fecd051994a0f9f451a31
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-04 01:27:47 +0530

    yess

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // 🔹 React router fallback (VERY IMPORTANT)
-app.get("/*", (req, res) => {
+app.get( (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
