022c94ebcebb56a67e00372030be701f3c8ef25a
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-08 22:57:38 +0530

    gugs inngest

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -2,38 +2,33 @@ import {Inngest} from "inngest"
 import { connectDB } from "./db.js"
 import User from "../models/User.js"
 
-export const inngest = new Inngest({id:"talend-iq"})
+export const inngest = new Inngest({id:"talent-iq"})
 
 const syncUser=inngest.createFunction(
 
         {id:"sync-user"},
-        {event:"cler/user.created"},
+        {event:"clerk/user.created"},
 
         async({event})=>{
             await connectDB()
 
-            const { id,emailAddresses,first_name, last_name,img_url }=event.data
+            const { id,email_addresses,first_name, last_name,image_url }=event.data
             const newUser={
                 clerkId:id,
-                email:emailAddresses[0]?.emailAddresses,
+                email:email_addresses[0].email_address,
                 name:`${first_name || " "} ${last_name}`,
-                profileImage:img_url
+                profileImage:image_url
             }
 await User.create(newUser)
 
 
         }
-
-
 )
-
-
-
 
 const deleteUserFromDb = inngest.createFunction(
 
     {id:"delete-user-from-db"},
-    {event:"cler/user.deleted"},
+    {event:"clerk/user.deleted"},
 
     async({event})=>{
         await connectDB()
