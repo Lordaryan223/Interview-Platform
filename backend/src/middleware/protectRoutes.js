9a8f3bdc9d0e12038137486ee0692a760e560262
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-14 00:47:21 +0530

    auth->auth()

--- backend/src/middleware/protectRoutes.js
+++ backend/src/middleware/protectRoutes.js
@@ -5,7 +5,7 @@ export const protectRoute=[
     requireAuth({signInUrl:"/sign-in"}),
     async(req,res,next)=>{
         try{
-            const clerkId= req.auth.userId
+            const clerkId= req.auth().userId
             if(!clerkId) return res.status(401).json({message:"unauthrized access"})
 
                 const user=await User.findOne({clerkId})
