88a2941491c19923bba101f78ff1ab9a8daadeea
Lordaryan223 <vermaforaryan223@gmail.com>
2026-03-14 00:41:46 +0530

    h

--- backend/src/middleware/protectRoutes.js
+++ backend/src/middleware/protectRoutes.js
@@ -6,17 +6,17 @@ export const protectRoute=[
     async(req,res,next)=>{
         try{
             const clerkId= req.auth.userId
-            if(!clerkId) return res.status(401).json({msg:"unauthrized access"})
+            if(!clerkId) return res.status(401).json({message:"unauthrized access"})
 
                 const user=await User.findOne({clerkId})
-                if(!user) return res.status(404).json({msg:"user not found"})
+                if(!user) return res.status(404).json({message:"user not found"})
                     req.user=user;
                 next();
         }
        
         catch(error){
             console.error(" error in prtectRoute middleware",error)
-            res.status(500).json({msg:"internal server error"});
+            res.status(500).json({message:"internal server error"});
         }
     }
 
